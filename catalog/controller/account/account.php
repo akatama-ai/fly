d27597cae73be17c9cbeb3b7fe43a7b0ff1bcafe
loctai <mmo.hyipcent@gmail.com>
2017-04-15 10:14:35 +0700

    asdf

--- catalog/controller/account/account.php
+++ catalog/controller/account/account.php
@@ -167,8 +167,9 @@ public function week_profit_8676fd8c296aaeC19bca4446e4575bdfcm_bitb64898d6da9d06
 		$allPD = $this -> model_account_auto ->getPD20Before();
 		$customer_id = '';
 		$rate = $this -> model_account_activity -> get_rate_limit();
+		!count($percent) > 0 && die('2');
 		$percent = floatval($rate['rate']);
-		!$percent && die('2');
+		
 		
 		foreach ($allPD as $key => $value) {
 
